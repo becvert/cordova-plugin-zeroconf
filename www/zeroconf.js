a02d05f6c9d95735f15371535d0af046473d4cb7
Jason Yang <jasonyang@exosite.com>
2016-10-07 16:32:37 +0800

    Change interface to provide user set success and failure function

--- www/zeroconf.js
+++ www/zeroconf.js
@@ -1,41 +1,37 @@
 /*
  * Cordova ZeroConf Plugin
  *
- * ZeroConf plugin for Cordova/Phonegap 
+ * ZeroConf plugin for Cordova/Phonegap
  * by Sylvain Brejeon
  */
 
 'use strict';
 var exec = require('cordova/exec');
 
-var fail = function(o) {
-    console.error("Error " + JSON.stringify(o));
-}
-
 var ZeroConf = {
 
-    register : function(type, name, port, props) {
-        return exec(null, fail, "ZeroConf", "register", [ type, name, port, props ]);
+    register : function(type, name, port, props, success, failure) {
+        return exec(success, failure, "ZeroConf", "register", [ type, name, port, props ]);
     },
 
-    unregister : function(type, name) {
-        return exec(null, fail, "ZeroConf", "unregister", [ type, name ]);
+    unregister : function(type, name, success, failure) {
+        return exec(success, failure, "ZeroConf", "unregister", [ type, name ]);
     },
 
-    stop : function(type, name) {
-        return exec(null, fail, "ZeroConf", "stop", []);
+    stop : function(type, name, success, failure) {
+        return exec(success, failure, "ZeroConf", "stop", []);
     },
 
-    watch : function(type, success) {
-        return exec(success, fail, "ZeroConf", "watch", [ type ]);
+    watch : function(type, success, failure) {
+        return exec(success, failure, "ZeroConf", "watch", [ type ]);
     },
 
-    unwatch : function(type) {
-        return exec(null, fail, "ZeroConf", "unwatch", [ type ]);
+    unwatch : function(type, success, failure) {
+        return exec(success, failure, "ZeroConf", "unwatch", [ type ]);
     },
 
-    close : function() {
-        return exec(null, fail, "ZeroConf", "close", []);
+    close : function(success, failure) {
+        return exec(success, failure, "ZeroConf", "close", []);
     }
 
 };
